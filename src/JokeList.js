25d2d75ffc12c9ef542ee3d2d5eabc5f7defd376
xjohnson <xjohnsondev@gmail.com>
2024-02-08 18:25:55 -0800

    refactored

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,43 +1,30 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Joke from "./Joke";
 import "./JokeList.css";
 
 /** List of jokes. */
 
-class JokeList extends Component {
-  static defaultProps = {
-    numJokesToGet: 5
-  };
+const JokeList = () => {
+  const NUM_JOKES_TO_GET = 5;
+  const [jokes, setJokes] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      jokes: [],
-      isLoading: true
-    };
+  //get jokes on start
+  useEffect(() => {
+    getJokes();
+  }, []);
 
-    this.generateNewJokes = this.generateNewJokes.bind(this);
-    this.vote = this.vote.bind(this);
-  }
-
-  /* at mount, get jokes */
-
-  componentDidMount() {
-    this.getJokes();
-  }
-
-  /* retrieve jokes from API */
-
-  async getJokes() {
+  // retrieve jokes from API
+  async function getJokes() {
     try {
       // load jokes one at a time, adding not-yet-seen jokes
       let jokes = [];
       let seenJokes = new Set();
 
-      while (jokes.length < this.props.numJokesToGet) {
+      while (jokes.length < NUM_JOKES_TO_GET) {
         let res = await axios.get("https://icanhazdadjoke.com", {
-          headers: { Accept: "application/json" }
+          headers: { Accept: "application/json" },
         });
         let { ...joke } = res.data;
 
@@ -48,63 +35,55 @@ class JokeList extends Component {
           console.log("duplicate found!");
         }
       }
-
-      this.setState({ jokes, isLoading: false });
+      setJokes((prevJokes) => [...prevJokes, ...jokes]);
+      setIsLoading(false);
     } catch (err) {
       console.error(err);
     }
   }
 
-  /* empty joke list, set to loading state, and then call getJokes */
+  // empty joke list, set to loading state, and then call getJokes
+  const generateNewJokes = () => {
+    setIsLoading(true);
+    getJokes();
+  };
 
-  generateNewJokes() {
-    this.setState({ isLoading: true});
-    this.getJokes();
+  // change vote for this id by delta (+1 or -1)
+  function vote(id, delta) {
+    setJokes((prevJokes) =>
+      prevJokes.map((j) => (j.id === id ? { ...j, votes: j.votes + delta } : j))
+    );
   }
-
-  /* change vote for this id by delta (+1 or -1) */
-
-  vote(id, delta) {
-    this.setState(st => ({
-      jokes: st.jokes.map(j =>
-        j.id === id ? { ...j, votes: j.votes + delta } : j
-      )
-    }));
-  }
-
-  /* render: either loading spinner or list of sorted jokes. */
-
-  render() {
-    let sortedJokes = [...this.state.jokes].sort((a, b) => b.votes - a.votes);
-    if (this.state.isLoading) {
-      return (
+  
+  // sort by upvote/ downvote differential
+  let sortedJokes = jokes.sort((a, b) => b.votes - a.votes);
+  
+  // render: either loading spinner or list of sorted jokes
+  return (
+    <div>
+      {isLoading ? (
         <div className="loading">
           <i className="fas fa-4x fa-spinner fa-spin" />
         </div>
-      )
-    }
+      ) : (
+        <div className="JokeList">
+          <button className="JokeList-getmore" onClick={generateNewJokes}>
+            Get New Jokes
+          </button>
 
-    return (
-      <div className="JokeList">
-        <button
-          className="JokeList-getmore"
-          onClick={this.generateNewJokes}
-        >
-          Get New Jokes
-        </button>
-
-        {sortedJokes.map(j => (
-          <Joke
-            text={j.joke}
-            key={j.id}
-            id={j.id}
-            votes={j.votes}
-            vote={this.vote}
-          />
-        ))}
-      </div>
-    );
-  }
-}
+          {sortedJokes.map((j) => (
+            <Joke
+              text={j.joke}
+              key={j.id}
+              id={j.id}
+              votes={j.votes}
+              vote={vote}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default JokeList;
